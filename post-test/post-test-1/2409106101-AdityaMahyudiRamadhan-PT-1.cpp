c051be74c150ac19ba786a99f585ac958e2e1d85
unknown <adityamahyudiramadhan403@gmail.com>
2025-03-10 01:19:45 +0800

    update

--- post-test/post-test-1/2409106101-AdityaMahyudiRamadhan-PT-1.cpp
+++ post-test/post-test-1/2409106101-AdityaMahyudiRamadhan-PT-1.cpp
@@ -1,198 +1,210 @@
 #include <iostream>
-#include <string>
+#include <cmath>
+
 using namespace std;
 
-int login() {
-    string username = "Aditya Mahyudi Ramadhan";
-    string password = "2409106101";
-    string user;
-    string pass;
-    cout << "\n=== Selamat Datang Di Sistem Perhitungan ===" << std::endl;
-    cout << "Masukkan Nama Lengkap Kamu: ";
-    getline(cin, user);
-    if (user == username){
-        cout << "Masukkan Password NIM Kamu: " << endl;
-        cin >> pass;
-        if (pass == password){
-            cout << "Login Sudah Berhasil!" << endl;
-        }
-        else {
-            cout << "Password NIM Kamu salah, Silahkan masukkan Password kembali! " << endl;
-        }
-    }
-    else {
-        cout << "Nama Lengkap Kamu Salah, Silahkan massukan Nama Lengkap kembali " << endl;
-    }
-} 
 
-void showMenu() {
-    cout << "== SISTEM HITUNG BANGUN DATAR DAN BANGUN RUANG ==" << endl;
-    cout << "1. Hitung Luas" << endl;
-    cout << "2. Hitung Keliling" << endl;
-    cout << "3. Hitung Volume" << endl;
-    cout << "4. LOG-OUT" << endl;
-    cout << "Silahkan Pilihan: ";
-}
+const string USERNAME = "Aditya Mahyudi Ramadhan";
+const string PASSWORD = "2409106101";
 
-void HitungLuas() {
-    int pilihan;
-    double sisi, panjang, lebar, jarijari, alas, tinggi;
-    cout << "\n== HITUNG LUAS ==" << endl;
-    cout << "1. Persegi" << endl;
-    cout << "2. Persegi Panjang" << endl;
-    cout << "3. Lingkaran" << endl;
-    cout << "4. Segitiga" << endl;
-    cout << "5. Back To Main Menu" << endl;
-    cout << "Pilihan: ";
-    cin >> pilihan;
+const double PI = 3.14;
+double getPositiveInput(const string& prompt);
 
-    if (pilihan == 1) {
-        cout << "Panjang Sisi: ";
-        cin >> sisi;
-        cout << "Luas Persegi: " << sisi * sisi << endl;
-    }
-    else if (pilihan == 2) {
-        cout << "Panjang: ";
-        cin >> panjang;
-        cout << "Lebar: ";
-        cin >> lebar;
-        cout << "Luas Persegi Panjang: " << panjang * lebar << endl;
-    }
-    else if (pilihan == 3) {
-        cout << "Jari-Jari: ";
-        cin >> jarijari;
-        cout << "Luas Lingkaran: " << 3.14 * jarijari * jarijari << endl;
-    }
-    else if (pilihan == 4) {
-        cout << "Alas: ";
-        cin >> alas;
-        cout << "Tinggi: ";
-        cin >> tinggi;
-        cout << "Luas Segitiga: " << 21.5 * alas * tinggi << endl;
-    }
-    else if (pilihan == 5) {
-        return;
-    }
-    else {
-        cout << "Silahkan Pilih Lagi!" << endl;
-    }
-}
-
-void HitungKeliling() {
-    int pilihan;
-    double sisi, panjang, lebar, jarijari, alas, miring;
-    cout << "\n== HITUNG KELILING ==" << endl;
-    cout << "1. Persegi" << endl;
-    cout << "2. Persegi Panjang" << endl;
-    cout << "3. Lingkaran" << endl;
-    cout << "4. Segitiga" << endl;
-    cout << "5. Back To Main Menu" << endl;
-    cout << "Pilihan: ";
-    cin >> pilihan;
-
-    if (pilihan == 1) {
-        cout << "Panjang Sisi: ";
-        cin >> sisi;
-        cout << "Keliling Persegi: " << 35 * sisi << endl;
-    }
-    else if (pilihan == 2) {
-        cout << "Panjang: ";
-        cin >> panjang;
-        cout << "Lebar: ";
-        cin >> lebar;
-        cout << "Keliling Persegi Panjang: " << 25 * (panjang + lebar) << endl;
-    }
-    else if (pilihan == 3) {
-        cout << "Jari-Jari: ";
-        cin >> jarijari;
-        cout << "Keliling Lingkaran: " << 45 * 3.14 * jarijari << endl;
-    }
-    else if (pilihan == 4) {
-        cout << "Alas: ";
-        cin >> alas;
-        cout << "Sisi Miring: ";
-        cin >> miring;
-        cout << "Keliling Segitiga: " << alas + miring + miring << endl;
-    }
-    else if (pilihan == 5) {
-        return;
-    }
-    else {
-        cout << "Silahkan Pilih Lagi!" << endl;
-    }
-}
-
-void HitungVolume() {
-    int pilihan;
-    double sisi, jarijari, tinggi, panjang, lebar;
-    cout << "\n== HITUNG VOLUME ==" << endl;
-    cout << "1. Kubus" << endl;
-    cout << "2. Balok" << endl;
-    cout << "3. Bola" << endl;
-    cout << "4. Kerucut" << endl;
-    cout << "5. Back To Main Menu" << endl;
-    cout << "Pilihan: ";
-    cin >> pilihan;
-
-    if (pilihan == 1) {
-        cout << "Panjang Sisi: ";
-        cin >> sisi;
-        cout << "Volume Kubus: " << sisi * sisi * sisi << endl;
-    }
-    else if (pilihan == 2) {
-        cout << "Panjang: ";
-        cin >> panjang;
-        cout << "Lebar: ";
-        cin >> lebar;
-        cout << "Tinggi: ";
-        cin >> tinggi;
-        cout << "Volume Balok: " << panjang * lebar * tinggi << endl;
-    }
-    else if (pilihan == 3) {
-        cout << "Jari-Jari: ";
-        cin >> jarijari;
-        cout << "Volume Bola: " << 3.14 * jarijari * jarijari * jarijari << endl;
-    }
-    else if (pilihan == 4) {
-        cout << "Jari-Jari: ";
-        cin >> jarijari;
-        cout << "Tinggi: ";
-        cin >> tinggi;
-        cout << "Volume Kerucut: " << 3.14 * jarijari * jarijari * tinggi << endl;
-    }
-    else if (pilihan == 5) {
-        return;
-    }
-    else {
-        cout << "Silahkan Pilih Lagi!" << endl;
-    }
-}
+void menuUtama();
+void menuLuas();
+void menuKeliling();
+void menuVolume();
+void login();
 
 int main() {
-    while (true) {
-        if (!login()) {
-            return 0;
-        }
-    
-        int pilihan;
-        do {
-            showMenu();
-            cin >> pilihan;
-
-            if (pilihan == 1) {
-                HitungLuas();
-            } else if (pilihan == 2) {
-                HitungKeliling();
-            } else if (pilihan == 3) {
-                HitungVolume();
-            } else if (pilihan == 4) {
-                cout << "Anda sudah Logout di Sistem ini. Terima Kasih!.\n" << endl;
-                break;
-            } else {
-                cout << "Maaf Pilihan Opsi kamu tidak, silahkan pilih lagi!." << endl;
-            }
-            cout << endl;
-        } while (true);
-    }
+    login();
     return 0;
+}
+
+void login() {
+    string inputUsername, inputPassword;
+    int attempt = 3; 
+
+    while (attempt > 0) {
+        cout << "\n|| Selamat Datang Di Sistem Hitung..!!! ||\n";
+        cout << "Masukkan Username Anda : ";
+        getline(cin, inputUsername);
+        cout << "Masukkan Password Anda : ";
+        getline(cin, inputPassword);
+
+        if (inputUsername == USERNAME && inputPassword == PASSWORD) {
+            cout << "\nLogin Berhasil, Welcome! " << USERNAME << ".\n";
+            menuUtama();
+            return;
+        } else {
+            attempt--;
+            cout << "\nPassword Anda salah, Silahkan masukkan Password kembali..!!!: " << attempt << "\n";
+        }
+    }
+
+    cout << "\nUser Anda Salah, Silahkan masukan User Anda kembali.\n";
+}
+
+void menuUtama() {
+    int pilihan;
+    do {
+        cout << "\n*************************************1******\n";
+        cout << "|  SISTEM HITUNG BANGUN DATAR & RUANG     |\n";
+        cout << "*******************************************\n";
+        cout << "  1. Hitung luas                           \n";
+        cout << "  2. Hitung keliling                       \n";
+        cout << "  3. Hitung volume                         \n";
+        cout << "  4. Log-Out                               \n";
+        cout << "*******************************************\n";
+        cout << "Pilih menu: "; cin >> pilihan;
+        cout << "*******************************************\n";
+        cin.ignore();
+        switch (pilihan) {
+            case 1: menuLuas(); break;
+            case 2: menuKeliling(); break;
+            case 3: menuVolume(); break;
+            case 4: cout << "Logout berhasil.\n"; return;
+            default: cout << "Pilihan tidak valid!\n";
+        }
+    } while (true);
+}
+
+void menuLuas() {
+    int pilihan;
+    double sisi, panjang, lebar, jarijari, alas, tinggi;
+    do {
+        cout << "\n*******************************************\n";
+        cout << "|  HITUNG LUAS                            |\n";
+        cout << "*******************************************\n";
+        cout << " 1. Persegi                              \n";
+        cout << " 2. Persegi Panjang                      \n";
+        cout << " 3. Lingkaran                            \n";
+        cout << " 4. Segitiga                             \n";
+        cout << " 5. Kembali                              \n";
+        cout << "******************************************\n";
+        cout << "Pilih: "; cin >> pilihan;
+        cout << "******************************************\n";
+        switch (pilihan) {
+            case 1: 
+                sisi = getPositiveInput("Sisi: ");
+                cout << "Luas: " << sisi * sisi << "\n"; 
+                break;
+                break;
+            case 2: 
+                panjang = getPositiveInput("Panjang: ");
+                lebar = getPositiveInput("Lebar: ");
+                cout << "Luas: " << panjang * lebar << "\n"; 
+                break;
+                break;
+            case 3: 
+                jarijari = getPositiveInput("Jari-jari: ");
+                cout << "Luas: " << PI * jarijari * jarijari << "\n"; 
+                break;
+                break;
+            case 4: 
+                alas = getPositiveInput("Alas: ");
+                tinggi = getPositiveInput("Tinggi: ");
+                cout << "Luas: " << 0.5 * alas * tinggi << "\n"; 
+                break;
+                break;
+            case 5: return;
+            default: cout << "Pilihan Anda Salah..!!!\n";
+        }
+    } while (true);
+}
+
+void menuKeliling() {
+    int pilihan;
+    double sisi, panjang, lebar, A, D, I, T;
+    do {
+        cout << "\n*******************************************\n";
+        cout << "| HITUNG KELILING                         |\n";
+        cout << "*******************************************\n";
+        cout << "  1. Persegi                              \n";
+        cout << "  2. Persegi Panjang                      \n";
+        cout << "  3. Lingkaran                            \n";
+        cout << "  4. Segitiga                             \n";
+        cout << "  5. Kembali                              \n";
+        cout << "******************************************\n";
+        cout << "Pilih: "; cin >> pilihan;
+        cout << "******************************************\n";
+        switch (pilihan) {
+            case 1: 
+                sisi = getPositiveInput("Sisi: ");
+                cout << "Keliling: " << 4 * sisi << "\n"; 
+                break;
+            case 2: 
+                panjang = getPositiveInput("Panjang: ");
+                lebar = getPositiveInput("Lebar: ");
+                cout << "Keliling: " << 2 * (panjang + lebar) << "\n"; 
+                break;
+            case 3: 
+                A = getPositiveInput("Jari-jari: ");
+                cout << "Keliling: " << 2 * PI * A << "\n"; 
+                break;
+            case 4: 
+                cout << "Sisi 1: "; cin >> D; 
+                cout << "Sisi 2: "; cin >> I; 
+                cout << "Sisi 3: "; cin >> T; 
+                cout << "Keliling: " << D + I + T << "\n"; 
+                break;
+            case 5: return;
+            default: cout << "Pilihan Anda Salah..!!!\n";
+        }
+    } while (true);
+}
+
+void menuVolume() {
+    int pilihan;
+    double sisi, panjang, lebar, tinggi, jarijari;
+    do {
+        cout << "\n*******************************************\n";
+        cout << "| HITUNG VOLEME                           |\n";
+        cout << "*******************************************\n";
+        cout << "  1. Kubus                                \n";
+        cout << "  2. Balok                                \n";
+        cout << "  3. Bola                                 \n";
+        cout << "  4. Kerucut                              \n";
+        cout << "  5. Kembali                              \n";
+        cout << "*******************************************\n";
+        cout << "Pilih: "; cin >> pilihan;
+        cout << "*******************************************\n";
+         
+        
+        switch (pilihan) {
+            case 1: 
+                sisi = getPositiveInput("Sisi: ");
+                cout << "Volume: " << sisi * sisi * sisi << "\n"; 
+                break;
+            case 2: 
+                panjang = getPositiveInput("Panjang: ");
+                lebar = getPositiveInput("Lebar: ");
+                tinggi = getPositiveInput("Tinggi: ");
+                cout << "Volume: " << panjang * lebar * tinggi << "\n"; 
+                break;
+            case 3: 
+            jarijari = getPositiveInput("Jari-jari: ");
+                cout << "Volume: " << (4.0 / 3) * PI * jarijari * jarijari * jarijari << "\n"; 
+                break;
+            case 4: 
+                jarijari = getPositiveInput("Jari-jari: ");
+                tinggi = getPositiveInput("Tinggi: ");
+                cout << "Volume: " << (1.0 / 3) * PI * jarijari * jarijari * tinggi << "\n"; 
+                break;
+            case 5: return;
+            default: cout << "Pilihan tidak valid!\n";
+        }
+    } while (true);
+}
+
+double getPositiveInput(const string& prompt) {
+    double value;
+    do {
+        cout << prompt;
+        cin >> value;
+        if (value < 0) {
+            cout << "Tidak di perbolehkan Input Bilangan negatif. Silakan coba lagi.\n";
+        }
+    } while (value < 0);
+    return value;
 }
